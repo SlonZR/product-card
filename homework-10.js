c2e6b3703a7b47895f0c4f893be3a9640d836ef7
SlonZR <rurus453@gmail.com>
2026-09-08 21:58:12 +0300

    fixed errorr

--- homework-10.js
+++ homework-10.js
@@ -7,33 +7,25 @@ function getCardCount() {
     let count;
     while (true) {
         const input = prompt("Сколько карточек отобразить? От 1 до 5");
-        
         if (input === null) return 0; 
-
         count = parseInt(input, 10);
-
         if (!isNaN(count) && count >= 1 && count <= 5) {
             return count; 
         }
         alert("Некорректный ввод! Пожалуйста, укажите число от 1 до 5.");
     }
 }
-
  function renderCards(cardsArray) {
     if (!productList || !productTemplate) return;
-    
     productList.innerHTML = '';
-
     cardsArray.forEach(product => {
         const productClone = productTemplate.content.cloneNode(true);
 
-        
         const img = productClone.querySelector('.product-image'); 
         if (img) {
             img.src = product.image; 
             img.alt = product.name;
         }
-        
         productClone.querySelector('.product-category').textContent = product.category;
         productClone.querySelector('.product-name').textContent = product.name;
         productClone.querySelector('.product-description').textContent = product.description;
@@ -53,25 +45,19 @@ function getCardCount() {
                 });
             });
         }
-
         productClone.querySelector('.product-price').innerHTML = `
             <span>Цена:</span>
             <span>${product.price} ₽</span>
         `;
-
         productList.appendChild(productClone);
     });
 }
 
 function init() {
-    
     const countToDisplay = getCardCount();
-
     const selectedProducts = productsCard.slice(0, countToDisplay);
-
     renderCards(selectedProducts);
 }
-
 init();
 
 const getProducts = Object.keys(productsCard)
